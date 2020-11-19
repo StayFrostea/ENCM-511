0a98be259316718498505020e24f3d73c241f0bd
Toshi Taperek <robotoshi@gmail.com>
2020-11-18 22:51:43 -0700

    Create project folder for Application1

---
+++ App1/App1.X/main.c
@@ -0,0 +1,14 @@
+/*
+ * File:   main.c
+ * Author: Toshi Taperek, Long Tran, Jeff Roszell
+ *
+ * Created on November 18, 2020, 10:36 PM
+ */
+
+
+#include "xc.h"
+
+int main(void)
+{
+	return 0;
+}
