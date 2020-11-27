d66e2f4ddb0e1fe7a18be1096d88045f7a55c2ac
StayFrostea <59980526+StayFrostea@users.noreply.github.com>
2020-11-27 14:49:19 -0700

    Update ADC.c
    I have wrote the setup and intialization of the ADC file.

--- Driver5/Driver5.X/ADC.c
+++ Driver5/Driver5.X/ADC.c
@@ -17,10 +17,33 @@ IFS0bits.AD1IF = 0; // Clear the ADC1 Interrupt Flag
 uint16_t do_ADC(void)
 {
 uint16_t ADCvalue; // 16 bit register used to hold ADC converted digital output ADC1BUF0
+uint16_t numLines;
 /* ------------- ADC INITIALIZATION ------------------*/
-ADCON1bits.ADON = 1; // turn on ADC module
+AD1CON1bits.ADON = 1; // turn on ADC module
+AD1CON1bits.FORM = 00; //This will set up integer
+AD1CON1bits.SSRC = 111; //Use internal counter for start/stop
+AD1CONbits.ASAM = 0;
 
-AD1CON3bits.ADRC = 0;
+AD1CON2bits.VCFG = 0;//System Clock
+AD1CON2bits.CSCNA = 000;//Do not scan results
+AD1CON2bits.SMPI = 0000;//Interupt at end of each conversion
+AD1CON2bits.BUFM = 0;//16 bit buffer
+AD1CON2bits.ALTS = 0;//Always use MUX A
+
+AD1CON3bits.ADRC = 0;//Clock for system derived on the system clock
+AD1CON3bits.SAMC = 1010;//Divide system clock by ten
+AD1CON3bits.ADCS = 00000;
+
+AD1CHSbits.CH0NB = 0;//Channel 0 Vr-
+AD1CHSbits.CH0SB = 0101;//The positive for channel 0 is on AN5
+AD1CHSbits.CH0NA = 0;//Channel 0 Vr-
+AD1CHSbits.CH0SA = 0101;//The positive for channel 0 is on AN5
+
+AD1PCFG = 0; //Enables use of ADC on AN5
+
+IFS0bits.AD1IF = 0;//Clear flag for ADC
+IPC3bits.AD1IP = 4;//Set priority level for interrupt
+IEC0bits.AD1IE = 1;//Enable the interrupt
 
 /* ------------- ADC SAMPLING AND CONVERSION ------------------*/
 AD1CON1bits.SAMP=1; //Start Sampling, Conversion starts automatically after SSRC and SAMC settings
