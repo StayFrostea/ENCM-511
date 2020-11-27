0fed863061f014539d31aced4d8198900392cb82
StayFrostea <59980526+StayFrostea@users.noreply.github.com>
2020-11-27 14:59:46 -0700

    Update main.c
    Got rid of what we don't need.

--- Driver5/Driver5.X/main.c
+++ Driver5/Driver5.X/main.c
@@ -8,52 +8,29 @@
 // MPLAB header libraries
 #include <xc.h>
 
-#include <p24fxxxx.h>
-#include <p24F16KA101.h>
-#include <stdio.h>
-#include <math.h>
-#include <errno.h>
-
 // User header files
 
 #include "ChangeClk.h"
 #include "TimeDelay.h"
 #include "IOs.h"
 #include "UART2.h"
+#include "ADC.h"
 
 #pragma config FCKSM = CSECMD // Clock switching is enabled, clock monitor disabled
 
-// Global variables
-uint8_t CNflag = 0;
-
-// MACROS for Idle, Sleep modes
-#define Nop() {__asm__ volatile ("nop");}
-#define ClrWdt() {__asm__ volatile ("clrwdt");}
-#define Sleep() {__asm__ volatile ("pwrsav #0");}   //Sleep() - put MCU in sleep mode - CPU and some peripherals off
-#define Idle() {__asm__ volatile ("pwrsav #1");}    //Idle() - put MCU in idle mode - only CPU off
-#define dsen() {__asm__ volatile ("BSET DSCON, #15");} //
-
 //MAIN
 void main(void) {
-    //Clock output on REFO/RB15 - Testing purposes only
-     TRISBbits.TRISB15 = 0;  // Set RB15 as output for REFO
-     REFOCONbits.ROEN = 1; // Ref oscillator is enabled
-     REFOCONbits.ROSSLP = 0; // Ref oscillator is disabled in sleep
-     REFOCONbits.ROSEL = 0; // Output base clk showing clock switching
-     REFOCONbits.RODIV = 0b0000;
      
     // Change Clock
-    NewClk(32); // 8 for 8 MHz; 500 for 500 kHz; 32 for 32 kHz
-     
-   // Initialize IOs for low-power wake-up
-    AD1PCFG = 0xFFFF; // Turn all analog pins as digital
-    IOinit();  // enables IO and CN interrupts on Push buttons
+    NewClk(500); // 8 for 8 MHz; 500 for 500 kHz; 32 for 32 kHz
     
+    uint16_t res;//For stroing the result of the conversion
+    
+    InitUART2();
 
     while(1){
-        IOmain();
+        res = do_ADC();
     }             
-    
     
     return;
 }
